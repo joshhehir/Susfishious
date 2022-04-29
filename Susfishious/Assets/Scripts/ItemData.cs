527f9699a3f86368e4eb1c936e3268bf53221bc6
PorridgeB <49393032+PorridgeB@users.noreply.github.com>
2022-04-29 14:21:32 +1000

    created item data SO

---
+++ Susfishious/Assets/Scripts/ItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
+{
+    public Sprite sprite;
+    public string name;
+    public string description;
+    public string itemMutation;
+}
